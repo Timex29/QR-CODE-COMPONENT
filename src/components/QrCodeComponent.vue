066c41b0d3d7f2c319f2d421ffd258c1155cf257
@Cornelio <jun23.llagas@gmail.com>
2024-08-20 10:44:34 +0800

    Update the QrCodeComponent.vue

--- src/components/QrCodeComponent.vue
+++ src/components/QrCodeComponent.vue
@@ -1,20 +1,16 @@
 <script setup>
-  import imageQR from "@/assets/images/image-qr-code.png";
+import imageQR from '@/assets/images/image-qr-code.png'
 </script>
 <template>
   <div class="container">
     <img :src="imageQR" alt="" />
     <h1>Improve your front-end skills by building projects</h1>
-    <p>
-      Scan the QR code to visit Frontend Mentor and take your coding skills to
-          the next level
-    </p>
+    <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
   </div>
- 
- <div class="attribution">
-  Challenge by
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
-        >Frontend Mentor</a
-      >. Coded by <a href="https://tsbsankara.netlify.app">Cornelio Llagas</a>.
- </div>
+
+  <div class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded
+    by <a href="https://github.com/Timex29/Qr-code-component" target="_blank">Cornelio Llagas</a>.
+  </div>
 </template>
